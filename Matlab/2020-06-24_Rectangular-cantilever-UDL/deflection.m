f4a7cbbae80493153ca2306aef4479810837d607
J.L.Symonds Patel <66869738+JAIMINLSP@users.noreply.github.com>
2020-07-02 12:32:35 +0100

    Add files via upload

---
+++ Matlab/2020-06-24_Rectangular-cantilever-UDL/deflection.m
@@ -0,0 +1,15 @@
+%% function to model the deflection of a cantilever beam from the: 
+% 1) The points along the beam as an array "x"
+% 2) The Second moment of area of the cross section of the beam "I"
+% 3) Young's modulus of the material "E"
+% 4) The length of the beam "L"
+% 5) The UDL applied on the whole length of the beam "w"
+%% the function returns:
+% An array containing the deflections matching the array "x"
+
+function def = deflection(x, I, E, L, w)
+% using the deflection equation (derived from the bending moment equation):
+def = (1/(E*I))*((L*w*x.^3/6)-(w*L^2*x.^2/4)-(w*x.^4/24));
+end
+
+%% JLSP
